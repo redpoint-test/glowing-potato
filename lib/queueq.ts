295bfe01161f5a465491c137dd46dd087775da71
Seth Law <seth.w.law@gmail.com>
2026-06-30 18:47:27 -0600

    tidy

--- lib/queueq.ts
+++ lib/queueq.ts
@@ -2,6 +2,6 @@ import { getDb, initializeSchema } from "./db.ts";
 
 export function searchQueueUnsafe(term: string) {
   initializeSchema();
-  const sql = `select id, title, status from tasks where title like '%${term}%' order by id desc`;
+  const sql = `select id, title, status from tasks where title like '%${term}%' order by id desc `;
   return getDb().prepare(sql).all();
 }
